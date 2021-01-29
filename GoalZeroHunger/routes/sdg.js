401e9cc763317c79683d823c8367ef6dfc207124
Ricardo Rodriguez <rodriguez.cando.ricardo.jamie@iisgalvanimi.edu.it>
2021-01-29 11:34:18 +0000

    1.2 tre route create

--- GoalZeroHunger/routes/sdg.js
+++ GoalZeroHunger/routes/sdg.js
@@ -15,7 +15,7 @@ router.get('/cereal', function (req, res, next) {
         if (err) console.log("Conessione al db non riuscita");
         else {
             const collection = client.db("WORLD_DATA_BANK_ZEROHUNGER").collection("cereal_yield");
-            collection.find().limit(10).toArray(callBackQuery);
+            collection.find().toArray(callBackQuery);
         }
     } 
 
@@ -27,4 +27,49 @@ router.get('/cereal', function (req, res, next) {
     
 });
 
+//per la collezione food-security
+router.get('/food-security', function (req, res, next) {
+
+    console.log(req.params);
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    client.connect(getListFood);  
+
+    function getListFood(err) {
+        if (err) console.log("Conessione al db non riuscita");
+        else {
+            const collection = client.db("WORLD_DATA_BANK_ZEROHUNGER").collection("food_security");
+            collection.find().toArray(callBackQuery);
+        }
+    } 
+
+    function callBackQuery(err, result)  {
+        if (err) console.log(err.message); 
+        else res.send(result);
+        client.close(); 
+    }
+    
+});
+
+//per la collezione undernourishment
+router.get('/undernourishment', function (req, res, next) {
+
+    console.log(req.params);
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    client.connect(getUndernourishment);  
+
+    function getUndernourishment(err) {
+        if (err) console.log("Conessione al db non riuscita");
+        else {
+            const collection = client.db("WORLD_DATA_BANK_ZEROHUNGER").collection("prevalence_of_undernourishment");
+            collection.find().toArray(callBackQuery);
+        }
+    } 
+
+    function callBackQuery(err, result)  {
+        if (err) console.log(err.message); 
+        else res.send(result);
+        client.close(); 
+    }
+    
+});
 module.exports = router;
