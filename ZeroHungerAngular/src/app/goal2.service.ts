b3881d2a3ca38fc8ddc00c597559b311d49027ea
Ricardo Rodriguez <rodriguez.cando.ricardo.jamie@iisgalvanimi.edu.it>
2021-02-04 20:35:39 +0000

    modifiche sul server e modifiche varie nel client

--- ZeroHungerAngular/src/app/goal2.service.ts
+++ ZeroHungerAngular/src/app/goal2.service.ts
@@ -9,7 +9,7 @@ export class Goal2Service {
   constructor(private http : HttpClient) { }
 
   visualCereal(query: string) {
-    const url = `https://3000-d35be42c-9894-4711-97d6-627f7bfbfc24.ws-eu03.gitpod.io/cereal/cereal_yield`;
+    const url = `https://3000-fb243d3a-1449-4e3d-b790-ef992c87403a.ws-eu03.gitpod.io/cereal/cereal_yield`;
     const headers = new HttpHeaders("");
     return this.http.get(url, { headers });
   }
