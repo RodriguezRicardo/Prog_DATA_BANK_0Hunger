ee4bd6152f3f30931de116e959aefd3a75d63161
Ricardo Rodriguez <rodriguez.cando.ricardo.jamie@iisgalvanimi.edu.it>
2021-02-04 20:51:23 +0000

    aggiunti metodi per richiamare i dati con servizio

--- ZeroHungerAngular/src/app/goal2.service.ts
+++ ZeroHungerAngular/src/app/goal2.service.ts
@@ -8,9 +8,22 @@ export class Goal2Service {
 
   constructor(private http : HttpClient) { }
 
-  visualCereal(query: string) {
+  visualCereal() {
     const url = `https://3000-fb243d3a-1449-4e3d-b790-ef992c87403a.ws-eu03.gitpod.io/cereal/cereal_yield`;
     const headers = new HttpHeaders("");
     return this.http.get(url, { headers });
   }
+
+  visualFoodSec() {
+    const url = `https://3000-fb243d3a-1449-4e3d-b790-ef992c87403a.ws-eu03.gitpod.io/food/food_security`;
+    const headers = new HttpHeaders("");
+    return this.http.get(url, { headers });
+  }
+
+  visualUndernourishment() {
+    const url = `https://3000-fb243d3a-1449-4e3d-b790-ef992c87403a.ws-eu03.gitpod.io/undernourishment/prevalence_of_undernourishment`;
+    const headers = new HttpHeaders("");
+    return this.http.get(url, { headers });
+  }
+
 }
