6e4f8a500deaffcd43064ed89021cfdf445e101c
Ricardo Rodriguez <rodriguez.cando.ricardo.jamie@iisgalvanimi.edu.it>
2021-02-09 07:54:19 +0000

    visual dati cereali nel comp.

--- ZeroHungerAngular/src/app/cereal/cereal.component.ts
+++ ZeroHungerAngular/src/app/cereal/cereal.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Goal2Service } from '../goal2.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-cereal',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cereal.component.css']
 })
 export class CerealComponent implements OnInit {
+  obsG : Observable<Object>;
+  res : any;
 
-  constructor() { }
+  constructor(public goal2 : Goal2Service) { }
 
   ngOnInit(): void {
+    this.obsG = this.goal2.visualCereal();
+    this.obsG.subscribe((data) => { this.res = data; console.log(this.res) });
   }
 
 }
--- ZeroHungerAngular/src/app/goal2.service.ts
+++ ZeroHungerAngular/src/app/goal2.service.ts
@@ -9,19 +9,19 @@ export class Goal2Service {
   constructor(private http : HttpClient) { }
 
   visualCereal() {
-    const url = `https://3000-brown-tortoise-ghcgfs3w.ws-eu03.gitpod.io/cereal/cereal_yield`;
+    const url = `https://3000-blue-galliform-ix5ggtg9.ws-eu03.gitpod.io/cereal/cereal_yield`;
     const headers = new HttpHeaders("");
     return this.http.get(url, { headers });
   }
 
   visualFoodSec() {
-    const url = `https://3000-brown-tortoise-ghcgfs3w.ws-eu03.gitpod.io/food/food_security`;
+    const url = `https://3000-blue-galliform-ix5ggtg9.ws-eu03.gitpod.io/food/food_security`;
     const headers = new HttpHeaders("");
     return this.http.get(url, { headers });
   }
 
   visualUndernourishment() {
-    const url = `https://3000-brown-tortoise-ghcgfs3w.ws-eu03.gitpod.io/undernourishment/prevalence_of_undernourishment`;
+    const url = `https://3000-blue-galliform-ix5ggtg9.ws-eu03.gitpod.io/undernourishment/prevalence_of_undernourishment`;
     const headers = new HttpHeaders("");
     return this.http.get(url, { headers });
   }
